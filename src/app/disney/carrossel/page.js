be0d6a0bec07abfefde0a7dcc22a4c498267a7db
Orion Teles de Mesquita <orionteles@gmail.com>
2024-09-02 20:11:16 -0300

    Carrossel

--- src/app/disney/carrossel/page.js
+++ src/app/disney/carrossel/page.js
@@ -3,23 +3,31 @@
 import Pagina from "@/app/components/Pagina";
 import { useEffect, useState } from "react";
 import apiDisney from "@/app/services/apiDisney";
+import { Carousel } from "react-bootstrap";
 
-export default function Page(){
-    
+export default function Page() {
+
     const [personagens, setPersonagens] = useState([])
 
-    useEffect(()=>{
-        apiDisney.get('character').then(resultado=>{
+    useEffect(() => {
+        apiDisney.get('character').then(resultado => {
             setPersonagens(resultado.data.data)
         })
     }, [])
-    
+
     return (
         <Pagina titulo="Disney Carrossel">
-            
-            {personagens.map(item => (
-                <p>{item.name}</p>
-            ))}
+
+            <Carousel className="mt-3" style={{width: 800}}>
+                {personagens.map(item => (
+                    <Carousel.Item key={item._id} >
+                        <img className="d-block auto" width={800} height={400} src={item.imageUrl} />
+                        <Carousel.Caption>
+                            <h3>{item.name}</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
 
         </Pagina>
     )
--- src/app/disney/tabela/page.js
+++ src/app/disney/tabela/page.js
@@ -3,23 +3,54 @@
 import Pagina from "@/app/components/Pagina";
 import { useEffect, useState } from "react";
 import apiDisney from "@/app/services/apiDisney";
+import { Table } from "react-bootstrap";
 
-export default function Page(){
-    
+export default function Page() {
+
     const [personagens, setPersonagens] = useState([])
 
-    useEffect(()=>{
-        apiDisney.get('character').then(resultado=>{
-            setPersonagens(resultado.data.data)
+    useEffect(() => {
+        apiDisney.get('character').then(resultado => {
+
+            // let res = resultado.data.data
+            // const a = res.sort(function(a,b) {
+            //     if(a.name < b.name) return -1;
+            //     if(a.name > b.name) return 1;
+            //     return 0;
+            // });
+
+            // console.log(a);
+
+            setPersonagens(resultado.data.data) 
         })
     }, [])
-    
+
     return (
         <Pagina titulo="Disney Tabela">
-            
-            {personagens.map(item => (
-                <p>{item.name}</p>
-            ))}
+
+            <Table className="mt-3" striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nome</th>
+                        <th>Foto</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {personagens.map(item => (
+                        <tr key={item._id}>
+                            <td>{item._id}</td>
+                            <td>{item.name}</td>
+                            <td>
+                                <img src={item.imageUrl} height={100} />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+
+
+
 
         </Pagina>
     )
